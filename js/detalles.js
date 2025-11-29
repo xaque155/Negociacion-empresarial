ac7a41bbff6ee6f1105aa3c7bf41cb5fd08708bb
Carlos Perez <xaque155@gmail.com>
2025-11-29 02:01:03 -0600

    creacion de paginados (esqueleto)

--- js/detalles.js
+++ js/detalles.js
@@ -1,17 +1,12 @@
 
-            fetch("./components/header.html")
+            fetch("../components/header.html")
         .then(resp => resp.text())
         .then(html => {
             document.getElementById("header").innerHTML = html;
         });
-        fetch("./components/navbar.html")
+        fetch("../components/navbar.html")
         .then(resp => resp.text())
         .then(html => {
             document.getElementById("navbar").innerHTML = html;
-        });
-        fetch("./components/banner.html")
-        .then(resp => resp.text())
-        .then(html => {
-            document.getElementById("banner").innerHTML = html;
         });
         
